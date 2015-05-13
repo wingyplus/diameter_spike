61275187f7a2badc0539ab95c500b3c6446ef588
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2015-05-13 16:13:39 +0700

    move Response to query_balance.go and implement Data interface

--- omr_diameter/query_balance.go
+++ omr_diameter/query_balance.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"time"
+
+	"github.com/fiorix/go-diameter/diam"
+	"github.com/fiorix/go-diameter/diam/avp"
+	"github.com/fiorix/go-diameter/diam/datatype"
+)
+
+type Balancer interface {
+	Retrieve(number SubscriberNumber) BalanceInformation
+}
+
+type DiamResponse struct {
+	ServiceInformation ServiceInformation
+}
+
+type ServiceInformation struct {
+	BalanceInformation BalanceInformation
+}
+
+type BalanceInformation struct {
+	FirstActiveDate string `json:"firstActiveDate"`
+}
+
 type QueryBalancer struct{}
 
 func (q *QueryBalancer) Retrieve(number SubscriberNumber) BalanceInformation {
@@ -14,7 +38,50 @@ func (q *QueryBalancer) Retrieve(number SubscriberNumber) BalanceInformation {
 var in chan Request
 
 type Request struct {
-	out chan Response
+	out  chan Response
+	data Data
+}
+
+type QueryBalanceData struct {
+	Number SubscriberNumber
+	Time   time.Time
+}
+
+func (d *QueryBalanceData) AVP() []*diam.AVP {
+	const (
+		balanceInformation  = 21100
+		accessMethod        = 20340
+		accountQueryMethod  = 20346
+		sspTime             = 20386
+		callingPartyAddress = 20336
+	)
+
+	number := string(d.Number)
+	balanceInfoRequest := diam.NewAVP(balanceInformation, avp.Mbit, 0, &diam.GroupedAVP{
+		AVP: []*diam.AVP{
+			diam.NewAVP(callingPartyAddress, avp.Mbit, 0, datatype.UTF8String(number)),
+			diam.NewAVP(accessMethod, avp.Mbit, 0, datatype.Unsigned32(9)),
+			diam.NewAVP(accountQueryMethod, avp.Mbit, 0, datatype.Unsigned32(1)),
+			diam.NewAVP(sspTime, avp.Mbit, 0, datatype.Time(d.Time)),
+		},
+	})
+	return []*diam.AVP{
+		diam.NewAVP(avp.SubscriptionID, avp.Mbit, 0, &diam.GroupedAVP{
+			AVP: []*diam.AVP{
+				diam.NewAVP(avp.SubscriptionIDType, avp.Mbit, 0, datatype.Integer32(0)),
+				diam.NewAVP(avp.SubscriptionIDData, avp.Mbit, 0, datatype.UTF8String(number)),
+			},
+		}),
+		diam.NewAVP(avp.ServiceInformation, avp.Mbit, 0, &diam.GroupedAVP{
+			AVP: []*diam.AVP{
+				balanceInfoRequest,
+			},
+		}),
+	}
+}
+
+type Data interface {
+	AVP() []*diam.AVP
 }
 
 type Response interface {
--- omr_diameter/query_balance_test.go
+++ omr_diameter/query_balance_test.go
@@ -3,7 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
+	"time"
+
+	"github.com/fiorix/go-diameter/diam"
+	"github.com/fiorix/go-diameter/diam/avp"
+	"github.com/fiorix/go-diameter/diam/datatype"
 )
 
 type mockDiameterClient struct {
@@ -12,9 +18,7 @@ type mockDiameterClient struct {
 
 func (m *mockDiameterClient) Run() {
 	request := <-in
-	if m.fn != nil {
-		m.fn(request.out)
-	}
+	m.fn(request.out)
 }
 
 func newClient(fn func(out chan Response)) *mockDiameterClient {
@@ -71,5 +75,46 @@ func TestBalancerCallDiameterClient(t *testing.T) {
 	expected := "20150101"
 	if balanceInfo.FirstActiveDate != expected {
 		t.Errorf("expect %s but got %s", expected, balanceInfo.FirstActiveDate)
+	}
+}
+
+func TestQueryBalanceData(t *testing.T) {
+	currentTime := time.Now()
+	request := QueryBalanceData{
+		Number: "66949014731",
+		Time:   currentTime,
+	}
+
+	const (
+		balanceInformation  = 21100
+		accessMethod        = 20340
+		accountQueryMethod  = 20346
+		sspTime             = 20386
+		callingPartyAddress = 20336
+	)
+
+	balanceInfoRequest := diam.NewAVP(balanceInformation, avp.Mbit, 0, &diam.GroupedAVP{
+		AVP: []*diam.AVP{
+			diam.NewAVP(callingPartyAddress, avp.Mbit, 0, datatype.UTF8String("66949014731")),
+			diam.NewAVP(accessMethod, avp.Mbit, 0, datatype.Unsigned32(9)),
+			diam.NewAVP(accountQueryMethod, avp.Mbit, 0, datatype.Unsigned32(1)),
+			diam.NewAVP(sspTime, avp.Mbit, 0, datatype.Time(currentTime)),
+		},
+	})
+	expected := []*diam.AVP{
+		diam.NewAVP(avp.SubscriptionID, avp.Mbit, 0, &diam.GroupedAVP{
+			AVP: []*diam.AVP{
+				diam.NewAVP(avp.SubscriptionIDType, avp.Mbit, 0, datatype.Integer32(0)),
+				diam.NewAVP(avp.SubscriptionIDData, avp.Mbit, 0, datatype.UTF8String("66949014731")),
+			},
+		}),
+		diam.NewAVP(avp.ServiceInformation, avp.Mbit, 0, &diam.GroupedAVP{
+			AVP: []*diam.AVP{
+				balanceInfoRequest,
+			},
+		}),
+	}
+	if !reflect.DeepEqual(request.AVP(), expected) {
+		t.Errorf("expect %v but got %v", expected, request.AVP())
 	}
 }
